9d3477dd4c29b72bf290a820804112d9ee3696cf
Kevin McPherson <kjmcpherson@gmail.com>
2018-01-10 00:53:25 +0000

    Done.

---
+++ lib/practice.rb
@@ -0,0 +1,12 @@
+Bill     = ["Bill", 44, "Hero", "555-555-5555"]
+Kevin    = ["Kevin", 33, "Musician","555-555-5555"]
+
+def print_business_card(person)
+  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
+  puts "Contact: #{person[3]}"
+end
+
+
+
+print_business_card(Kevin)
+print_business_card(Kevin)
